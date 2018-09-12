03638db8863544e34580ed4ff82aece2e975b5a4
Aline Gouveia <38138702+alinemtg@users.noreply.github.com>
2018-09-12 17:43:52 -0300

    Add files via upload
    algumas mudanças aaa

--- lista2.cpp
+++ lista2.cpp
@@ -8,27 +8,21 @@ struct node {
     node *next;
 
     // CONSTRUTOR DO NODE
-    node (string nome, int id){
-        nome = nome;
-        id = id;
-        next = NULL;
-    }
+    node(string nome, int id): nome(nome), id(id), next(nullptr) {}
 };
 
 // ~~~ FILA ~~~
 struct fila {
     node *front;
     node *rear;
+    int tam;
 
     // CONSTRUTOR DA FILA
-    fila (){
-        front = NULL;
-        rear = NULL:
-    }
+    fila (): front(nullptr), rear(nullptr), tam(0) {}
 
     void add (string nome, int id){
         node *aux = new node (nome, id);
-        if (this->rear != NULL){
+        if (this->rear){
             this->rear->next = aux;
             this->rear = aux;
         }else{
@@ -39,15 +33,42 @@ struct fila {
 
     node* remove (){
         node *aux = this->front;
-        if (this->front != NULL){
+        if (this->front){
             this->front = this->front->next;
         }
+        tam--;
         return aux;
     }
 };
 
+// ~~~ METODOS DA QUESTAO ~~~
+
+int buscaBinaria (int ids[], int idProcurado){
+    int iniAux = 0;
+    int fimAux = (sizeof(ids)/sizeof(int))-1;
+    while (iniAux<=fimAux){
+        int posMeio = (iniAux+fimAux)/2;
+        if (idProcurado == ids[posMeio]){
+            return posMeio;
+        }else if (idProcurado<ids[posMeio]){
+            fimAux = posMeio-1;
+        }else{
+            iniAux = posMeio+1;
+        }
+    }
+    return -1;
+}
+
 int dispersao (int chave, int quantGavetas){
     return (chave%quantGavetas);
+}
+
+int obterChave (string nome){
+    int chave = 0;
+    for (int i=0; i<nome.length(); i++){
+        chave+=(i*nome[i]);
+    }
+    return chave;
 }
 
 int main (){
@@ -58,7 +79,6 @@ int main (){
 
     string arquivosInicioNome [quantArquivosInicial];
     int arquivosInicioId [quantArquivosInicial];
-
     for (int i=0; i<quantArquivosInicial; i++){
         string nome;
         int id;
@@ -68,40 +88,25 @@ int main (){
     }
 
     int quantGavetas, quantArquivosSeraoTransf;
-    cin >> quantGavetas >> quantArquivosSeraoTransf;
-
-    int arquivosSeraoTransf[quantArquivosSeraoTransf];
+    cin >> quantGavetas;
+    cin >> quantArquivosSeraoTransf;
+    fila gavetas [quantGavetas];
     for (int i=0; i<quantArquivosSeraoTransf; i++){
-        int idAux;
-        cin >> idAux;
-        arquivosSeraoTransf[i] = idAux;
+        int idProcurado;
+        cin >> idProcurado;
+        int posArqTransf = buscaBinaria(arquivosInicioId, idProcurado);
+        cout << posArqTransf;
+        gavetas[dispersao(obterChave(arquivosInicioNome[posArqTransf]),quantGavetas)].add(arquivosInicioNome[posArqTransf], arquivosInicioId[posArqTransf]);
     }
 
-    // PROCURAR ARQUIVOS QUE SERAO TRANSF. E ARMAZENAR NAS GAVETAS
-//    fila gavetas [quantGavetas];
-//    for (int i=0; i<quantArquivosSeraoTransf; i++){
-//        for (int j=0; j<quantArquivosInicial; j++){
-//            if (arquivosSeraoTransf[i] == arquivosInicioId[j]){
-//                int chave;
-//                for (int c=0; c<arquivosInicioNome[j].length(); c++){
-//                    k+=(c*arquivosInicioNome[j][c]);
-//                }
-//                gaveta = dispersao(chave, quantGavetas);
-//                if (gaveta<gavetas.length()){
-//                    gavetas[gaveta].add(arquivosInicioNome[j], arquivosInicioId[j]);
-//                }else{
-//
-//                }
-//            }
-//        }
-//    }
+    // IMPRIMIR QUANTOS ARQUIVOS TEM EM CADA GAVETA
+    for (int i=0; i<quantGavetas; i++){
+        cout << i << ": " << gavetas[i].tam << endl;
+    }
 
-
-
-
+    // ANALISE DA EFICIENCIA, OU SEJE, DE CATAR OS ARQUIVOS
     int quantArquivosConsult;
     cin >> quantArquivosConsult;
-    // FAZER O FOR DA LEITURA DOS ARQUIVOS CONSULTADOS
 
 
 
