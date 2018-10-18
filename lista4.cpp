901287bb2b2958e186dc3b6929140f3e8b22f160
Aline Gouveia <38138702+alinemtg@users.noreply.github.com>
2018-10-18 09:54:13 -0300

    lista4

--- lista4.cpp
+++ lista4.cpp
@@ -1,3 +1,6 @@
+/* O Hospital Universitário 
+ https://iudex.io/problem/5bb8204ffcc3820001202507/statement */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,57 +10,140 @@ struct paciente {
     int idade;
     int ordemChegada;
 
-    heap (int urgencia, int idade, int ordemChegada): urgencia(urgencia), idade(idade), ordemChegada(ordemChegada) {}
-}*
+    paciente (int urgencia, int idade, int ordemChegada): urgencia(urgencia), idade(idade), ordemChegada(ordemChegada) {}
+};
 
-void inserir (paciente* fila[], int tamFila, int urgencia, int idade, int ordemChegada){
-    paciente* pacienteAux = new paciente (urgencia, idade, ordemChegada);
-    int capacidade = sizeof(fila)/ sizeof(fila[0])
+void bubbleUp (paciente* fila[], int posicaoProblematica){
+    while (posicaoProblematica>0 && (  (fila[posicaoProblematica]->urgencia > fila[(int)floor((posicaoProblematica-1)/2)]->urgencia) ||
+                                       (fila[posicaoProblematica]->urgencia == fila[(int)floor((posicaoProblematica-1)/2)]->urgencia && fila[posicaoProblematica]->idade > fila[(int)floor((posicaoProblematica-1)/2)]->idade) ||
+                                       (fila[posicaoProblematica]->urgencia == fila[(int)floor((posicaoProblematica-1)/2)]->urgencia && fila[posicaoProblematica]->idade == fila[(int)floor((posicaoProblematica-1)/2)]->idade
+                                        && fila[posicaoProblematica]->ordemChegada < fila[(int)floor((posicaoProblematica-1)/2)]->ordemChegada) )){
+        paciente* pacienteAux = fila[posicaoProblematica];
+        fila[posicaoProblematica] = fila[(int)floor((posicaoProblematica-1)/2)];
+        fila[(int)floor((posicaoProblematica-1)/2)] = pacienteAux;
 
-    if (tamFila == capacidade){
-        paciente* filaAux [capacidade*2];
-        for (int i=0; i<tamFila; i++){
-            filaAux[i] = fila[i];
-        }
-        fila = filaAux;
+        posicaoProblematica = (int)(floor((posicaoProblematica-1) / 2));
     }
+}
+
+void inserir (paciente* fila[], int tamFila, int capacFila, int urgencia, int idade, int ordemChegada){
+    auto * pacienteAux = new paciente (urgencia, idade, ordemChegada);
 
     fila[tamFila] = pacienteAux;
-    fila.bubbleUp(fila, tamFila-1);
+    bubbleUp(fila, tamFila);
 }
 
-void bubbleUp (paciente* fila, int posicaoProblematica){
-    while (posicaoProblematica>0 && fila[posicaoProblematica].urgencia>fila[(posicaoProblematica-1)/2]){
-        paciente* pacienteAux = fila[posicaoProblematica];
-        fila[posicaoProblematica] = fila[(posicaoProblematica-1)/2];
-        fila[(posicaoProblematica-1)/2] = pacienteAux;
+void heapify (paciente* fila[], int tamFila, int posicao){
+    int l = 2*posicao+1;
+    int r = 2*posicao+2;
+    int m = posicao;
 
-        posicaoProblematica = (posicaoProblematica-1)/2
+    if (l<tamFila && (  (fila[l]->urgencia > fila[m]->urgencia) || (fila [l]->urgencia == fila[m]->urgencia && fila[l]->idade > fila[m]->idade) ||
+                        (fila[l]->urgencia == fila[m]->urgencia && fila[l]->idade == fila[m]->idade && fila[l]->ordemChegada < fila[m]->ordemChegada) ) ){
+        m = l;
     }
+    if (r<tamFila && (  (fila[r]->urgencia > fila[m]->urgencia) || (fila [r] ->urgencia == fila[m]->urgencia && fila[r]->idade > fila[m]->idade) ||
+                        (fila[r]->urgencia == fila[m]->urgencia && fila[r]->idade == fila[m]->idade && fila[r]->ordemChegada < fila[m]->ordemChegada) ) ){
+        m = r;
+    }
+    if (m!=posicao){
+        paciente* pacienteAux = fila [posicao];
+        fila[posicao] = fila[m];
+        fila[m] = pacienteAux;
+        heapify(fila, tamFila, m);
+    }
+
 }
 
+paciente* retirar (paciente* fila[], int tamFila){
+    paciente* pacienteAux = fila[tamFila-1];
+    fila[tamFila-1] = fila[0];
+    fila[0] = pacienteAux;
+    tamFila -= 1;
+    heapify (fila, tamFila, 0);
+    return fila[tamFila];
+}
 
 int main (){
     int numServicos;
     cin >> numServicos;
-
-    paciente* filasServico[numServicos][5];
     int tamFilas [numServicos];
+    int capacidadeFilas[numServicos];
+    paciente** filasServicos[numServicos];
 
     for (int i=0; i<numServicos; i++){
-        string evento;
-        cin >> evento;
+        tamFilas[i] = 0;
+        capacidadeFilas[i] = 4;
+        filasServicos[i] = new paciente* [4];
+    }
+
+    string evento;
+    int ordemChegada = 0;
+
+    while(cin >> evento && evento!= "END"){
+
         if (evento == "CIN"){
             int idade, urgencia, servico;
             cin >> idade >> urgencia >> servico;
 
-        }else if (evento == "NXT"){
+            if (tamFilas[servico] == capacidadeFilas[servico]){
+                auto ** filaAux = new paciente* [capacidadeFilas[servico]*2];
+                for (int i=0; i<tamFilas[servico]; i++){
+                    filaAux[i] = filasServicos[servico][i];
+                }
+                filasServicos[servico] = filaAux;
+                capacidadeFilas[servico] = capacidadeFilas[servico]*2;
+            }
+
+            inserir(filasServicos[servico], tamFilas[servico], capacidadeFilas[servico], urgencia, idade, ordemChegada);
+            tamFilas[servico] += 1;
+            ordemChegada ++;
+        }
+
+        if (evento == "STD"){
+            int servicoAtendido = -1;
+            int aux = -1;
+            auto * pacienteAtendido = new paciente (-1, -1, -1);
+            for (int i=0; i<numServicos; i++){
+                if (tamFilas[i] > 0){
+                    servicoAtendido = i;
+                    aux = i;
+                    break;
+                }
+            }
+            if (servicoAtendido != -1) {
+                for (int i=aux+1; i<numServicos; i++){
+                    if (tamFilas[i] > 0 && (
+                            (filasServicos[i][0]->urgencia < filasServicos[servicoAtendido][0]->urgencia) ||
+                            (filasServicos[i][0]->urgencia == filasServicos[servicoAtendido][0]->urgencia && filasServicos[i][0]->idade < filasServicos[servicoAtendido][0]->idade) ||
+                            (filasServicos[i][0]->urgencia == filasServicos[servicoAtendido][0]->urgencia && filasServicos[i][0]->idade == filasServicos[servicoAtendido][0]->idade &&
+                             filasServicos[i][0]->ordemChegada < filasServicos[servicoAtendido][0]->ordemChegada))) {
+                        servicoAtendido = i;
+                    }
+                }
+                pacienteAtendido = retirar(filasServicos[servicoAtendido], tamFilas[servicoAtendido]);
+                tamFilas[servicoAtendido] -= 1;
+            }
+            cout << servicoAtendido << " " << pacienteAtendido->idade << " " << pacienteAtendido->urgencia << endl;
+        }
+
+        if (evento == "NXT"){
             int fila;
             cin >> fila;
-        }else{
-            //atendimento estudantes
+            int servicoAtendido = -1;
+            auto * pacienteAtendido = new paciente (-1, -1, -1);
+
+            if (tamFilas[fila]>0){
+                pacienteAtendido = retirar(filasServicos[fila], tamFilas[fila]);
+                servicoAtendido = fila;
+                tamFilas[fila] -= 1;
+            }
+            cout << servicoAtendido << " " << pacienteAtendido->idade << " " << pacienteAtendido->urgencia << endl;
         }
+
+
     }
 
     return 0;
 }
+
